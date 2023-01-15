11fc64d8cd74571e8749ad6b06133e3ee0a864e8
lxl66566 <18259734087@163.com>
2023-01-15 23:50:25 +0800

    20230115-23:50:25

--- Cpp_temp.cpp
+++ Cpp_temp.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
+#include <queue>
 #define ll long long
 using namespace std;
-
+priority_queue<int> q;
 int main()
 {
 	ios::sync_with_stdio(0);
     cin.tie(nullptr);
-    cout << "hello,world!" ;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    
     return 0;
 }
