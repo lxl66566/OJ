8956a6c112c4da5cf34a073281efc1adc9c33616
lxl66566 <18259734087@163.com>
2023-01-20 01:58:10 +0800

    20230120-01:58:09

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+typedef long long ll;
+const ll MOD = 998244353;
+int main()
+{
+    cout << -36 % MOD << endl;
+    return 0;
+}
